79aea3e9147d65778c97dc28e0d6d4ad968b9fec
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-10-24 09:35:16 +0100

    implement the main method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.showCoordinates();
-        Point p2 = new Point(5,7);
-        p2.showCoordinates();
+        //get the maximum number of points in the space from the keyboard
+
+        System.out.println("Enter the maximum number of points : ");
+        Scanner sc=new Scanner(System.in);
+        int nbMax=sc.nextInt();
+
+        //create a space with the maximum number of points (instantiation)
+        Space2D space=new Space2D(nbMax);
+        space.setTabPoints();
+        System.out.println("The space is : ");
+        space.printSpace();
+        //find a point in the space
+        Point p=new Point();
+        p.getCoordinates();
+        int index=space.findPoint(p);
+        if(index==-1)
+            System.out.println("The point is not in the space");
+        else
+            System.out.println("The point is on the index : "+index);
+
     }
 }
