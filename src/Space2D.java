6e7a26aea2c3ed5bc14801d91f7704b8410b25d5
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-10-24 14:42:00 +0100

    implement the main method

--- src/Space2D.java
+++ src/Space2D.java
@@ -18,19 +18,29 @@ public class Space2D {
     //print all points in the space (array) to the screen
     public void printSpace()
     {
-
+        for (int i = 0; i < nbPoints; i++) {
+            tabPoints[i].showCoordinates();
+        }
     }
     //get some points information from the keyboard and add them to the space(to the array)
     public void setTabPoints()
     {
-
+        for (int i = 0; i < tabPoints.length; i++) {
+            tabPoints[i]=new Point();
+            tabPoints[i].getCoordinates();
+        }
     }
     //find a given point in the space (array)
     //return the index of the point in the array
     //return -1 if the point is not in the space
     public int findPoint(Point p)
     {
-
+        int i =0;
+        while (i<tabPoints.length && !p.compare(tabPoints[i]))
+            i++;
+        if(i<tabPoints.length)
+            return i;
+        return -1;
     }
 
 }
